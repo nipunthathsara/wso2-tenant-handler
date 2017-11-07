526c1b91ffe0d85bcac4e013ca73859a60a6e9fd
nipunthathsara <nipunthathsara@gmail.com>
2017-11-08 01:17:26 +0530

    cleaning code

--- src/main/java/org/wso2/sample/tenant/mgt/listener/util/EmailSendingUtil.java
+++ src/main/java/org/wso2/sample/tenant/mgt/listener/util/EmailSendingUtil.java
@@ -1,23 +1,5 @@
-/*
- * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- *
- * WSO2 Inc. licenses this file to you under the Apache License,
- * Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 package org.wso2.sample.tenant.mgt.listener.util;
 
-import org.wso2.carbon.identity.notification.mgt.AbstractNotificationSendingModule;
 import org.wso2.carbon.identity.notification.mgt.NotificationManagementException;
 import org.wso2.carbon.identity.notification.mgt.bean.PublisherEvent;
 import org.wso2.carbon.identity.notification.mgt.email.EmailModuleConstants;
@@ -39,52 +21,56 @@ import org.apache.commons.logging.LogFactory;
 import java.util.Map;
 import java.util.HashMap;
 
-/**
- * Created by nipun on Nov, 2017
- */
-public class EmailSendingUtil extends AbstractNotificationSendingModule implements Runnable {
+public class EmailSendingUtil implements Runnable {
     private static final Log log = LogFactory.getLog(EmailSendingUtil.class);
-    private static OMElement payload;
-    private static ServiceClient serviceClient;
+    private ServiceClient serviceClient;
+    private PublisherEvent publisherEvent;
 
-    public void sendMessage(PublisherEvent publisherEvent) throws NotificationManagementException {
-
-        payload = OMAbstractFactory.getOMFactory().createOMElement(BaseConstants
-                .DEFAULT_TEXT_WRAPPER, null);
-        String message = publisherEvent.getEventProperties().getProperty(NotificationUtil.BODY);
-        payload.setText(message);
-        ConfigurationContext configContext = CarbonConfigurationContextFactory.getConfigurationContext();
-
-        Map<String, String> headerMap = new HashMap<String, String>();
-        headerMap.put(MailConstants.MAIL_HEADER_SUBJECT, publisherEvent.getEventProperties()
-                .getProperty(MailConstants.MAIL_HEADER_SUBJECT));
-
-        try {
-            if (configContext != null) {
-                serviceClient = new ServiceClient(configContext, null);
-            } else {
-                serviceClient = new ServiceClient();
-            }
-            Options options = new Options();
-            options.setProperty(Constants.Configuration.ENABLE_REST, Constants.VALUE_TRUE);
-            options.setProperty(MessageContext.TRANSPORT_HEADERS, headerMap);
-            options.setProperty(MailConstants.TRANSPORT_MAIL_FORMAT,
-                    MailConstants.TRANSPORT_FORMAT_TEXT);
-            options.setTo(new EndpointReference(EmailModuleConstants.MAILTO_LABEL + publisherEvent.getEventProperties()
-                    .getProperty(MailConstants.MAIL_HEADER_TO)));
-            serviceClient.setOptions(options);
-            (new Thread(new EmailSendingUtil())).start();
-        } catch (AxisFault axisFault) {
-            log.error("Error while sending out tenant registration notification email" + axisFault);
-        }
+    public EmailSendingUtil(PublisherEvent publisherEvent) {
+        this.publisherEvent = publisherEvent;
     }
 
-    @Override
+    /**
+     * @return
+     * @throws NotificationManagementException
+     * This method returns the built payload and used
+     * in the run() method within the same class
+     */
+    private OMElement sendMessage() throws AxisFault {
+
+        ConfigurationContext configContext = CarbonConfigurationContextFactory.getConfigurationContext();
+        OMElement payload = OMAbstractFactory.getOMFactory().createOMElement(BaseConstants
+                .DEFAULT_TEXT_WRAPPER, null);
+        //Set body of the email
+        String message = publisherEvent.getEventProperties().getProperty(NotificationUtil.BODY);
+        payload.setText(message);
+        Map<String, String> headerMap = new HashMap();
+        //Set subject of the email
+        headerMap.put(MailConstants.MAIL_HEADER_SUBJECT, publisherEvent.getEventProperties()
+                .getProperty(MailConstants.MAIL_HEADER_SUBJECT));
+        if (configContext != null) {
+            serviceClient = new ServiceClient(configContext, null);
+        } else {
+            serviceClient = new ServiceClient();
+        }
+        Options options = new Options();
+        options.setProperty(Constants.Configuration.ENABLE_REST, Constants.VALUE_TRUE);
+        options.setProperty(MessageContext.TRANSPORT_HEADERS, headerMap);
+        options.setProperty(MailConstants.TRANSPORT_MAIL_FORMAT,
+                MailConstants.TRANSPORT_FORMAT_TEXT);
+        //Set receiver's address
+        options.setTo(new EndpointReference(EmailModuleConstants.MAILTO_LABEL + publisherEvent.getEventProperties()
+                .getProperty(MailConstants.MAIL_HEADER_TO)));
+        serviceClient.setOptions(options);
+        return payload;
+    }
+
     public void run() {
         try {
+            OMElement payload = sendMessage();
             serviceClient.fireAndForget(payload);
-        } catch (AxisFault axisFault) {
-            log.error("Error while sending out tenant registration notification email" + axisFault);
+        } catch (AxisFault e) {
+            log.error("Error while sending out tenant registration notification email" + e);
         }
     }
 }
--- src/main/java/org/wso2/sample/tenant/mgt/listener/util/NotificationUtil.java
+++ src/main/java/org/wso2/sample/tenant/mgt/listener/util/NotificationUtil.java
@@ -1,57 +1,32 @@
-/*
- * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- *
- * WSO2 Inc. licenses this file to you under the Apache License,
- * Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 package org.wso2.sample.tenant.mgt.listener.util;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.axis2.transport.mail.MailConstants;
 import org.wso2.carbon.identity.notification.mgt.NotificationManagementException;
 import org.wso2.carbon.identity.notification.mgt.bean.PublisherEvent;
-import org.apache.axis2.transport.mail.MailConstants;
 
-/**
- * Created by nipun on Nov, 2017
- */
 public class NotificationUtil {
     private static final Log log = LogFactory.getLog(NotificationUtil.class);
     private PublisherEvent event;
     public static final String BODY = "body";
 
-    public NotificationUtil (String email, String name, String domain){
+    public NotificationUtil(String email, String name, String domain) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("Hi " + name + ",\n");
         stringBuilder.append("Tenant domain: " + domain + " successfully registered. \n");
         stringBuilder.append("Thanks.");
-        try{
+        try {
             event = new PublisherEvent("TenantOperation");
             event.addEventProperty(MailConstants.MAIL_HEADER_TO, email);
             event.addEventProperty(MailConstants.MAIL_HEADER_SUBJECT, "Tenant domain registration");
             event.addEventProperty(BODY, stringBuilder.toString());
-        } catch (NotificationManagementException e){
+        } catch (NotificationManagementException e) {
             log.error("Error while creating publisher event " + e);
         }
     }
 
-    public void sendMail(){
-        EmailSendingUtil emailSendingUtil = new EmailSendingUtil();
-        try {
-            emailSendingUtil.sendMessage(this.event);
-        }catch (NotificationManagementException e){
-            log.error("Error while sending notification " + e);
-        }
+    public void sendNotification() {
+        (new Thread(new EmailSendingUtil(event))).start();
     }
 }
